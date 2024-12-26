c816af5f62bae7caf7c4cbe942bf4252fa414aad
QuatTrinh <quattrinh25111999@gmail.com>
2024-12-26 22:32:48 +0700

    Add dotenv dependency to use process.env

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 const express = require('express') // commonjs
 const path = require('path')
+require('dotenv').config()
 // import express from 'express' es modules
 
 const app = express()
-const port = 8080
+const port = process.env.PORT || 8080
+const hostname = process.env.HOST_NAME
 
 // config template engine
 app.set('views', path.join(__dirname, 'views'))
@@ -14,6 +16,6 @@ app.get('/', (req, res) => {
     res.render('sample.ejs')
 })
 
-app.listen(port, () => {
+app.listen(port, hostname, () => {
     console.log(`Example app listening on port ${port}`)
 })
