41d3311943eaaa6599353b5eaa2463444bfd4344
QuatTrinh <quattrinh25111999@gmail.com>
2024-12-27 22:21:04 +0700

    Seperate code

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,9 +1,7 @@
 const express = require('express')
 const router = express.Router()
+const { getHomePage } = require('../controllers/homeController')
 
-router.get('/', (req, res) => {
-    // res.send('Hello World!')
-    res.render('sample.ejs')
-})
+router.get('/', getHomePage)
 
 module.exports = router
