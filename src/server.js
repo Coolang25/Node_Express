8f46368c244417f6d805f5e2cacc1aab127f8b83
QuatTrinh <quattrinh25111999@gmail.com>
2024-12-27 22:11:00 +0700

    Route

--- src/server.js
+++ src/server.js
@@ -1,23 +1,17 @@
+require('dotenv').config()
 const express = require('express') // commonjs
 const path = require('path')
-require('dotenv').config()
-// import express from 'express' es modules
+const configViewEngine = require('./config/viewEngine')
+const webRoutes = require('./routes/web')
 
 const app = express()
 const port = process.env.PORT || 8080
 const hostname = process.env.HOST_NAME
 
-// config template engine
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
+// config view engine
+configViewEngine(app)
 
-// config static files
-app.use(express.static(path.join(__dirname, 'public')))
-
-app.get('/', (req, res) => {
-    // res.send('Hello World!')
-    res.render('sample.ejs')
-})
+app.use('/', webRoutes)
 
 app.listen(port, hostname, () => {
     console.log(`Example app listening on port ${port}`)
