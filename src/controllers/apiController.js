2a229a90cfaffca806fdd50b3f89b1b9ba593f58
QuatTrinh <quattrinh25111999@gmail.com>
2024-12-30 21:51:55 +0700

    crud

--- src/controllers/apiController.js
+++ src/controllers/apiController.js
@@ -12,6 +12,41 @@ const getUsersAPI = async (req, res) => {
     })
 }
 
+const postCreateUserAPI = async (req, res) => {
+    let { email, name, city } = req.body
+
+    let user = await User.create({ email, name, city })
+
+    return res.status(200).json({
+        errorCode: 0,
+        data: user
+    })
+}
+
+const putUpdateUserAPI = async (req, res) => {
+    let { email, name, city, id } = req.body
+
+    let user = await User.updateOne({ _id: id }, { email, name, city }).exec()
+
+    return res.status(200).json({
+        errorCode: 0,
+        data: user
+    })
+}
+
+const deleteUserAPI = async (req, res) => {
+    const userId = req.body.id;
+    let result = await User.deleteOne({ _id: userId }).exec()
+
+    return res.status(200).json({
+        errorCode: 0,
+        data: result
+    })
+}
+
 module.exports = {
     getUsersAPI,
+    postCreateUserAPI,
+    putUpdateUserAPI,
+    deleteUserAPI
 }
